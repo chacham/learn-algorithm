9f240a44fef0d04ae8398a6d8e25e00e81247449
chacham <ccm0702@naver.com>
2018-09-12 20:49:07 +0900

    Add omitted acmicpc.net/10866 solution

---
+++ acmicpc.net/10866/10866.py
@@ -0,0 +1,78 @@
+class deque:
+    def __init__(self, size):
+        self._start = 0
+        self._end = 0
+        self._size = 0
+        self._maxSize = size
+        self._deque = [None] * size
+    def push_front(self, item):
+        if self._size >= self._maxSize:
+            return
+        self._deque[self._start] = item
+        self._start -= 1
+        if self._start < 0:
+            self._start += self._maxSize
+        self._size += 1
+    def pop_front(self):
+        if self._size <= 0:
+            return -1
+        self._start += 1
+        if self._start >= self._maxSize:
+            self._start -= self._maxSize
+        self._size -= 1
+        return self._deque[self._start]
+    def push_back(self, item):
+        if self._size >= self._maxSize:
+            return
+        self._end += 1
+        if self._end >= self._maxSize:
+            self._end -= self._maxSize
+        self._deque[self._end] = item
+        self._size += 1
+    def pop_back(self):
+        if self._size <= 0:
+            return -1
+        ret = self._deque[self._end]
+        self._end -= 1
+        if self._end < 0:
+            self._end += self._maxSize
+        self._size -= 1
+        return ret
+    def size(self):
+        return self._size
+    def empty(self):
+        return True if self._size == 0 else False
+    def front(self):
+        if self._size <= 0:
+            return -1
+        tmp = self._start + 1
+        if tmp >= self._maxSize:
+            tmp -= self._maxSize
+        return self._deque[tmp]
+    def back(self):
+        if self._size <= 0:
+            return -1
+        return self._deque[self._end]
+
+if __name__ == "__main__":
+    N = int(input())
+    mydeq = deque(N)
+    for n in range(N):
+        op = input().split()
+        #print(mydeq._deque)
+        if op[0] == "push_front":
+            mydeq.push_front(op[1])
+        elif op[0] == "push_back":
+            mydeq.push_back(op[1])
+        elif op[0] == "pop_front":
+            print(mydeq.pop_front())
+        elif op[0] == "pop_back":
+            print(mydeq.pop_back())
+        elif op[0] == "size":
+            print(mydeq.size())
+        elif op[0] == "empty":
+            print(1 if mydeq.empty() else 0)
+        elif op[0] == "front":
+            print(mydeq.front())
+        elif op[0] == "back":
+            print(mydeq.back())
