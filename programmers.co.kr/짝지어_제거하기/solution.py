c43be78fbaf6660a772389b08d89ea8c65297ee2
chacham <ccm0702@naver.com>
2018-09-19 12:42:12 +0900

    Solved removing pair at programmers.co.kr

---
+++ programmers.co.kr/짝지어_제거하기/solution.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    return 0 if stack else 1
+
+if __name__ == "__main__":
+    print(solution(input()))
