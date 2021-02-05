5eed3db82d63fb917b2a7d86a5d8bcd9a03610e0
chacham <ccm0702@naver.com>
2021-02-05 22:35:35 +0900

    Solved WILDCARD at algospot.com

---
+++ algospot.com/WILDCARD/solve_without_dp.py
@@ -0,0 +1,34 @@
+def check(p, s):
+    def rec(i, j):
+        if i == len(p):
+            if j == len(s):
+                return True
+            return False
+        if p[i] == '*':
+            if j < len(s):
+                return rec(i, j+1) or rec(i+1, j)
+            return rec(i+1, j)
+        if j == len(s):
+            return False
+        if p[i] == '?' and j < len(s):
+            return rec(i+1, j+1)
+        if p[i] == s[j]:
+            return rec(i+1, j+1)
+        return False
+    return rec(0, 0)
+
+
+if __name__ == "__main__":
+    C = int(input())
+    for c in range(C):
+        W = input()
+        N = int(input())
+        FILES = [input() for n in range(N)]
+
+        res = []
+        for file in FILES:
+            if check(W, file):
+                res.append(file)
+
+        for file in sorted(res):
+            print(file)
